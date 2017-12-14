a243e9bc6f0ee7211240200380304388ca95bfc3
sarika <sarikaresume@gmail.com>
2017-12-13 16:57:26 -0800

    hello

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FriendsList from './components/index'
+import FriendsList from './components/FriendsList';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,4 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+       <FriendsList />
+
+      </div>
+    );
+
+  }
         
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getFriends } from '../actions';
+import axios from 'axios';
+
 class FriendsList extends Component {
+    constructor() {
+        super();
+
+
+    }
+
+    componentDidMount() {
+        this.props.getFriends();
+    }
+
+    
